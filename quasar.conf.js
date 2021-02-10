e87c70912f6730e463d5925ba42dceb4d9d9310f
Thayana Mamoré <thayana.mamore@topi.io>
2021-02-10 16:25:09 -0300

    Reload & Update Automatically

--- quasar.conf.js
+++ quasar.conf.js
@@ -115,7 +115,10 @@ module.exports = function (/* ctx */) {
     // https://quasar.dev/quasar-cli/developing-pwa/configuring-pwa
     pwa: {
       workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
-      workboxOptions: {}, // only for GenerateSW
+      workboxOptions: {
+        skipWaiting: true,
+        clientsClaim: true
+      }, // only for GenerateSW
       manifest: {
         name: 'FestMA',
         short_name: 'FestMA',
